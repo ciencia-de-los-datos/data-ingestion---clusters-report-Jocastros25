d4b1dbf49cc74ec51a50df72ab10f38f9c05b815
Jocastros25 <147558884+Jocastros25@users.noreply.github.com>
2023-10-21 20:52:49 -0500

    agrega todas las respuestas

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,57 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
+    df = pd.read_fwf('clusters_report.txt', 
+                     widths=[7,16,16,80], 
+                     names=['Cluster', 'Cantidad de palabras clave', 'Porcentaje de palabras clave', 'Principales palabras clave'], 
+                     skiprows=[0, 1, 2, 3],
+                     skip_footer=True,
+                     keep_default_na=False,
+                     na_filter=True,
+                     verbose=True,
+                     warn_bad_lines=False
+                     )
+    
+    for index, element in enumerate(df['Cluster']):
+        if element == '':
+            df.loc[index, 'Cluster'] = df.loc[index - 1, 'Cluster']
+            df.loc[index, 'Cantidad de palabras clave'] = df.loc[index - 1, 'Cantidad de palabras clave']
+            df.loc[index, 'Porcentaje de palabras clave'] = df.loc[index - 1, 'Porcentaje de palabras clave']
 
-    #
-    # Inserte su código aquí
-    #
+    df.columns = df.columns.str.replace(' ', '_')
+    df.columns = df.columns.str.lower()
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace("%", '').str.replace(",", '.').astype(float)
+
+    for name in df.columns:
+        df[name] = df[name].apply(lambda value: " ".join(str(value).strip().split()))
+
+    df['cluster'] = df['cluster'].astype(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(float)
+
+    df = df.groupby(['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave'])['principales_palabras_clave'].apply(' '.join).reset_index()
+
+    for index, element in enumerate(df['principales_palabras_clave']):
+        if element.endswith('.'):
+            df.loc[index, 'principales_palabras_clave'] = element[:-1]
 
     return df
+
+
+
+
+df = pd.read_fwf('clusters_report.txt', 
+                            widths=[7,16,16,80], 
+                            names =['Cluster', 'Cantidad de palabras clave', 'Porcentaje de palabras clave', 'Principales palabras clave'], 
+                            skiprows = [0,1,2,3],
+                            skip_footer     = True,
+                            keep_default_na = False,
+                            na_filter = True,
+                            verbose  = True,
+                            warn_bad_lines  = False,
+                            )
+
+#print(df["Principales palabras clave"].head())
+# Imprime el DataFrame resultante
+#print(df)
